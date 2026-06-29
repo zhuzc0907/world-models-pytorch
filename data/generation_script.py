2bbc812c1c03efc2b5ef2879f3f2a9afb4775efb
zhuzc0907 <zhuzc0907@outlook.com>
2026-06-29 13:41:57 +0800

    install required enviroments on MacOS

--- data/generation_script.py
+++ data/generation_script.py
@@ -7,30 +7,42 @@ import argparse
 from multiprocessing import Pool
 from subprocess import call
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--rollouts', type=int, help="Total number of rollouts.")
-parser.add_argument('--threads', type=int, help="Number of threads")
-parser.add_argument('--rootdir', type=str, help="Directory to store rollout "
-                    "directories of each thread")
-parser.add_argument('--policy', type=str, choices=['brown', 'white'],
-                    help="Directory to store rollout directories of each thread",
-                    default='brown')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rollouts', type=int, help="Total number of rollouts.")
+    parser.add_argument('--threads', type=int, help="Number of threads")
+    parser.add_argument('--rootdir', type=str, help="Directory to store rollout "
+                        "directories of each thread")
+    parser.add_argument('--policy', type=str, choices=['brown', 'white'],
+                        help="Directory to store rollout directories of each thread",
+                        default='brown')
+    args = parser.parse_args()
 
-rpt = args.rollouts // args.threads + 1
+    rpt = args.rollouts // args.threads  # 修改1: 去掉 +1，精确分配
 
-def _threaded_generation(i):
+    with Pool(args.threads) as p:
+        # 修改2: 使用 partial 或 lambda 将 args 传递给子函数
+        from functools import partial
+        p.map(partial(_threaded_generation, args=args, rpt=rpt), range(args.threads))
+
+    
+def _threaded_generation(i, args, rpt):  # 修改3: 增加 args 和 rpt 参数
     tdir = join(args.rootdir, 'thread_{}'.format(i))
     makedirs(tdir, exist_ok=True)
-    cmd = ['xvfb-run', '-s', '"-screen 0 1400x900x24"']
-    cmd += ['--server-num={}'.format(i + 1)]
-    cmd += ["python", "-m", "data.carracing", "--dir",
-            tdir, "--rollouts", str(rpt), "--policy", args.policy]
-    cmd = " ".join(cmd)
-    print(cmd)
-    call(cmd, shell=True)
-    return True
+    
+    # 修改4: 使用列表形式传递参数，避免字符串拼接和引号问题
+    cmd = [
+        # 'xvfb-run', # comment out since I'm using MacOS
+        # '-s', '-screen 0 1400x900x24',  # 去掉多余的嵌套引号
+        # '--server-num={}'.format(i + 1),
+        'python', '-m', 'data.carracing',
+        '--dir', tdir,
+        '--rollouts', str(rpt),
+        '--policy', args.policy
+    ]
+    
+    print(' '.join(cmd))
+    call(cmd)  # 修改5: 去掉 shell=True，直接传列表更安全
 
-
-with Pool(args.threads) as p:
-    p.map(_threaded_generation, range(args.threads))
+if __name__ == '__main__':
+    main()
